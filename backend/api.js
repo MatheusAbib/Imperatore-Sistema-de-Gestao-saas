b78f6d5ba1745ee4bd3937add7406b631751c325
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2026-06-25 10:01:00 -0300

    fix: ajusta api.js e vercel.json para deploy

--- backend/api.js
+++ backend/api.js
@@ -4,3 +4,10 @@ dotenv.config();
 const app = require('./src/app');
 
 module.exports = app;
+
+if (require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
+}
