aef81ebce44b7fda34676cee32f6fb56f164b855
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2026-06-26 08:31:15 -0300

    feat: adiciona tela de comandas finalizadas com modal de detalhes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//     baseURL: 'https://imperatore-zeta.vercel.app/_/backend/api',
-// });
-
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://imperatore-zeta.vercel.app/_/backend/api',
 });
+
+// const api = axios.create({
+//     baseURL: 'http://localhost:3000/api',
+// });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
